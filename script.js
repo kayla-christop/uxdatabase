9d295623af2f741aa44230717029aeca4090e196
kayla-christop <75920904+kayla-christop@users.noreply.github.com>
2021-01-16 18:46:21 -0500

    fix

--- script.js
+++ script.js
@@ -193,10 +193,6 @@ window.addEventListener("load", async function(event) {
             checkbox.checked=false;
         });
     });
-
-    function clearIt() {
-        document.getElementById("filter").value = "";
-    }
 });
 
 /*
