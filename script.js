1acd600354dcbc4089660cac6d21738d0a689b85
kayla-christop <75920904+kayla-christop@users.noreply.github.com>
2021-01-25 02:58:03 -0500

    change

--- script.js
+++ script.js
@@ -211,7 +211,7 @@ window.addEventListener("load", async function(event) {
         document.getElementById("bottomtext").style["border-color"] = 
         "white";
         document.getElementById("about").style.opacity = 1;
-        document.getElementById("homepage").style.transform = "translateY(-140px)";
+        document.getElementById("homepage").style.transform = "translateY(-160px)";
         };
       };
 });
