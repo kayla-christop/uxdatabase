ffc072cbadd2fce60cc0120393e7ad4c86755e97
kayla-christop <75920904+kayla-christop@users.noreply.github.com>
2021-03-25 00:53:33 -0400

    added a missing parantheses

--- script.js
+++ script.js
@@ -261,7 +261,7 @@ window.addEventListener("load", async function(event) {
       document.getElementById("backButton").addEventListener("click",function(event){
           document.getElementById("helpVideo").style.display = "none";
           document.getElementById("about").style.display = "block";
-      }
+      });
 });
 
 /*
