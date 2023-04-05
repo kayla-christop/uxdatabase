8327f3d8b89fedb6d55e923dabe5c43aba183b8d
kayla-christop <75920904+kayla-christop@users.noreply.github.com>
2023-04-04 21:12:44 -0500

    Updated Database.json path

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.addEventListener("load", async function(event) {
     setTimeout(function(){
         document.getElementById("bottomtext").click();
     },2500)
-    let database = await (await fetch("/Database.json")).json();
+    let database = await (await fetch("./Database.json")).json();
 
     let body = document.getElementById("body");
 
